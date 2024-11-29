88fe89f0414d4eb3712f793010783154f357ef8b
Muhammad Muzammil Loya <73660510+Muzammil-cyber@users.noreply.github.com>
2024-11-29 20:34:57 +0500

    Enhance Hero, Introduction, and LogoTicker sections with animations and scroll effects; implement framer-motion for improved user experience

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,25 +1,99 @@
+"use client";
 import Button from "@/components/Button";
 import designExample1Image from "@/assets/images/design-example-1.png";
 import designExample2Image from "@/assets/images/design-example-2.png";
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
+import { useAnimate, motion } from "framer-motion";
+import { useEffect } from "react";
+import customCursor from "@/assets/images/cursor-you.svg";
 
 export default function Hero() {
+  const [leftDesignScope, leftDesignAnimate] = useAnimate();
+  const [rightDesignScope, rightDesignAnimate] = useAnimate();
+  const [andreaPointerScope, andreaPointerAnimate] = useAnimate();
+  const [bryanPointerScope, bryanPointerAnimate] = useAnimate();
+
+  useEffect(() => {
+    leftDesignAnimate([
+      [leftDesignScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [leftDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+    ]);
+    rightDesignAnimate([
+      [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+      [rightDesignScope.current, { x: 0, y: 0 }, { duration: 0.5 }],
+    ]);
+    andreaPointerAnimate([
+      [andreaPointerScope.current, { opacity: 1 }, { duration: 0.5 }],
+      [andreaPointerScope.current, { x: -100, y: 0 }, { duration: 0.5 }],
+      [
+        andreaPointerScope.current,
+        { x: 0, y: [0, 16, 0] },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+
+    bryanPointerAnimate([
+      [
+        bryanPointerScope.current,
+        { opacity: 1 },
+        { duration: 0.5, delay: 1.5 },
+      ],
+      [bryanPointerScope.current, { x: 0, y: 100 }, { duration: 0.5 }],
+      [
+        bryanPointerScope.current,
+        { x: [0, 16, 0], y: 0 },
+        { duration: 0.5, ease: "easeInOut" },
+      ],
+    ]);
+  }, []);
+
   return (
-    <section className="py-24 overflow-x-clip">
+    <section
+      className="py-24 overflow-x-clip"
+      style={{ cursor: `url(${customCursor.src}), auto` }}
+    >
       <div className="container relative">
-        <div className="absolute -left-60 top-16 hidden lg:block">
-          <Image src={designExample1Image} alt="" placeholder="blur" />
-        </div>
-        <div className="absolute -right-[380px] -top-16  hidden lg:block">
-          <Image src={designExample2Image} alt="" placeholder="blur" />
-        </div>
-        <div className="absolute left-56 top-96  hidden lg:block">
+        <motion.div
+          ref={leftDesignScope}
+          initial={{ opacity: 0, x: -200, y: 200 }}
+          className="absolute -left-60 top-16 hidden lg:block"
+          drag
+        >
+          <Image
+            src={designExample1Image}
+            alt=""
+            placeholder="blur"
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          ref={rightDesignScope}
+          initial={{ opacity: 0, x: 200, y: 200 }}
+          className="absolute -right-[380px] -top-16  hidden lg:block"
+          drag
+        >
+          <Image
+            src={designExample2Image}
+            alt=""
+            placeholder="blur"
+            draggable={false}
+          />
+        </motion.div>
+        <motion.div
+          ref={andreaPointerScope}
+          initial={{ opacity: 0, y: 100, x: -200 }}
+          className="absolute left-56 top-96  hidden lg:block"
+        >
           <Pointer name="Andrea" />
-        </div>
-        <div className="absolute right-80 -top-4  hidden lg:block ">
+        </motion.div>
+        <motion.div
+          ref={bryanPointerScope}
+          initial={{ opacity: 0, y: 100, x: 200 }}
+          className="absolute right-80 -top-4  hidden lg:block "
+        >
           <Pointer name="Bryan" color="red" />
-        </div>
+        </motion.div>
         <div className="flex justify-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
             ✨ $7.5M seed round raised
